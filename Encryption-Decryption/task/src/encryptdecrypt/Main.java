7bff7519c6a3036089693a4c3cfb6d88dbbb2cb6
Nikolay Panin <nickolay.panin@gmail.com>
2020-07-02 21:33:11 +0400

    Complete 5 stage

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,5 +1,8 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +10,9 @@ public class Main {
         String direction = "enc";
         int key = 0;
         String data = "";
+        String inputSourceFile = "";
+        String outputSourceFile = "";
+        boolean dir;
         if (args.length > 0) {
             for (int i = 0; i < args.length; i+=2) {
                 switch (args[i]) {
@@ -19,34 +25,67 @@ public class Main {
                     case "-mode":
                         direction = args[i+1];
                         break;
+                    case "-in":
+                        inputSourceFile = args[i+1];
+                        break;
+                    case "-out":
+                        outputSourceFile = args[i+1];
+                        break;
                 }
             }
         }
 
-        switch (direction) {
-            case "enc":
-                System.out.println(encryption(data, key));
-                break;
-            case "dec":
-                System.out.println(decryption(data, key));
-                break;
-            default:
-                System.out.println("Unknown operation!");
+        File inputFile = new File(inputSourceFile);
+        File outputFile = new File(outputSourceFile);
+        dir = "enc".equals(direction);
+        System.out.println(deEncryption(data, key, dir));
+        if (!"".equals(inputSourceFile) && "".equals(data)) {
+            if ("".equals(outputSourceFile)) {
+                try (Scanner scanner = new Scanner(inputFile)) {
+                    while (scanner.hasNext()) {
+                        System.out.println(deEncryption(scanner.nextLine(), key, dir));
+                    }
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            } else {
+                try (Scanner scanner = new Scanner(inputFile);
+                PrintWriter printWriter = new PrintWriter(outputFile)) {
+                    while (scanner.hasNext()) {
+                        printWriter.println(deEncryption(scanner.nextLine(), key, dir));
+                    }
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
 
-
+            }
+        } else {
+            if ("".equals(outputSourceFile)) {
+                System.out.println(deEncryption(data, key, dir));
+            } else {
+                try (PrintWriter printWriter = new PrintWriter(outputFile)) {
+                    printWriter.println(deEncryption(data, key, dir));
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
         }
 
 
     }
 
+
+
+
     public static char charEncryption(char iChar, int shift) {
-        //return (char)((iChar + shift - 97) % 26 + 97);
-        return  (char)(iChar + shift);
+          return  (char)(iChar + shift);
     }
     public static char charDecryption(char iChar, int shift) {
         return (char)(iChar - shift);
     }
-    public static String encryption(String input, int key) {
+
+    public static String deEncryption(String input, int key, boolean direction) {
+        key = direction ? key : -key;
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < input.length(); i++) {
             result.append(charEncryption(input.charAt(i),key));
@@ -54,11 +93,5 @@ public class Main {
         return result.toString();
     }
 
-    public static String decryption(String input, int key) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            result.append(charDecryption(input.charAt(i),key));
-        }
-        return result.toString();
-    }
+
 }
